c5cbabd51d10e8caacbe50b1e480dd9440259f99
blindlobstar <boryapictures@gmail.com>
2021-02-01 22:32:09 +0300

    Add projects for communication through rabbitmq

---
+++ src/Basics/EventBus.RabbitMq/QueueAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventBus.RabbitMq
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class QueueAttribute : Attribute
+    {
+        private readonly string _routingKey;
+
+        public QueueAttribute(string routingKey)
+        {
+            _routingKey = routingKey;
+        }
+
+        public virtual string RoutingKey => _routingKey;
+    }
+}
---
+++ src/Basics/EventBus.RabbitMq/RabbitMqOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace EventBus.RabbitMq
+{
+    public class RabbitMqOptions
+    {
+        public string ConnectionString { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int RetryCount { get; set; }
+    }
+}
